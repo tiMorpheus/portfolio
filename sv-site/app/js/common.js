15cdc28e46252be53345cf900702e8484bf5ac3e
tiMorpheus <timur10645@icloud.com>
2018-01-11 15:51:30 +0200

    cv mastered

--- sv-site/app/js/common.js
+++ sv-site/app/js/common.js
@@ -1,5 +1,4 @@
-
-$(document).ready(function($) {
+$(document).ready(function ($) {
 
 
     /*======= Skillset *=======*/
@@ -7,20 +6,51 @@ $(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
 
-        $('.level-bar-inner').each(function() {
-
+        $('.level-bar-inner').each(function () {
             var itemWidth = $(this).data('level');
 
             $(this).animate({
                 width: itemWidth
             }, 2000);
-
         });
-
     });
 
+    var counter = 0;
+
+    $("button.load-test").bind('click', function () {
+
+        $.getJSON("projects.json", function (data) {
+
+
+
+            $("button.load-test").before(
+                '<div class="item">' +
+                '<span class="project-title counter-'+counter+'"><a href=' + data.projects[counter].link + '></a></span> - ' +
+                '<span class="project-tagline">' + data.projects[counter].description + '</span>' +
+                '</div>'
+            );
+
+            $('.counter-'+counter+' a').html(data.projects[counter].title);
+            counter++;
+
+            if (counter >= data.projects.length){
+
+                $("button.load-test").remove();
+                $(".intro p").after(
+                    '<a href="https://github.com/timorpheus">Code style and other projects you can find on my github</a>'
+                ).remove();
+            }
+        })
+    });
+
+
+    AOS.init({
+        easing: 'ease-out-back',
+        duration: 1000,
+
+    });
 
 
 });
