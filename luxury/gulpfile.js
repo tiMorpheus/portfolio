02d76db74662acd02270ad48d7c707b783100f16
tiMorpheus <timur10645@icloud.com>
2018-02-07 11:18:07 +0200

    init

---
+++ luxury/gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
+
+
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server:{
+            baseDir: 'app'
+        },
+        notify: false
+    })
+});
+
+
+gulp.task('watch',['browserSync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/lib/**/*.css', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
---
+++ sticky-header/README.md
@@ -0,0 +1,3 @@
+<h1>Sticky header</h1>
+
+
